0e08578d6e5377d103eea139f9454c73a1c6480b
jackdavidweber <jackdavidweber@gmail.com>
2020-05-26 11:22:15 -0400

    make first round of customizations to template to have it be personalized

--- portfolio/src/main/webapp/assets/js/main.js
+++ portfolio/src/main/webapp/assets/js/main.js
@@ -63,11 +63,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    new GitHubCalendar("#github-graph", "jackdavidweber");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "jackdavidweber", selector: "#ghfeed" });
 
 
 });
