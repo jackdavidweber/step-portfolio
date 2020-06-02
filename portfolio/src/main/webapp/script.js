85744becdbba50c6aa819539fb045a7bb5547737
jackdavidweber <jackdavidweber@gmail.com>
2020-06-02 23:22:53 +0000

    tell user how many comments are visible

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -69,6 +69,11 @@ async function getComments() {
       commentsListElement.appendChild(createListElement(commentsArr[i]));
 
   }
+
+  const numCommentsElement = document.getElementById('numComments');
+  numCommentsElement.innerHTML = "<p>Number of Comments Visible: "+ commentsArr.length.toString() + "</p>"
+
+
 }
 
 /** Creates an <li> element containing text. */
