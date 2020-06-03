f701f50103206df7aa8b538b3a498bc25df786b3
jackdavidweber <jackdavidweber@gmail.com>
2020-06-03 19:24:51 +0000

    make whitespacing more consistent

--- portfolio/src/main/webapp/assets/js/main.js
+++ portfolio/src/main/webapp/assets/js/main.js
@@ -62,10 +62,8 @@ jQuery(document).ready(function($) {
         }
     );
     
-    
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "jackdavidweber", selector: "#ghfeed" });
-
 
 });
 
