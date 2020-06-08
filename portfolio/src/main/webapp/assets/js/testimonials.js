e14997377febb4b9a14b35fda5b93faeb2cf6e77
jackdavidweber <jackdavidweber@gmail.com>
2020-06-08 17:13:15 +0000

    move all testimonials related functions to separate js file

---
+++ portfolio/src/main/webapp/assets/js/testimonials.js
@@ -0,0 +1,66 @@
+async function getTestimonials() {
+  const response = await fetch('/data');
+  const arrTestimonials = await response.json();
+  
+  const testimonialsListElement = document.getElementById('testimonials');
+  testimonialsListElement.innerHTML = '';
+
+  for (let i = 0; i < arrTestimonials.length; i++) {
+      testimonialsListElement.appendChild(createTestimonialElement(arrTestimonials[i]["text"], arrTestimonials[i]["name"], arrTestimonials[i]["title"]));
+  }
+}
+
+function createTestimonialElement(text,author,title){
+    // // create elements within block quote	
+    const blockQuoteParagraphElement = document.createElement('p');	
+    const iTagElement = document.createElement('i');
+    iTagElement.setAttribute("class","fas fa-quote-left");
+    const blockQuoteText = document.createTextNode(text);
+
+    // create block quote wrapper and add all elements to it
+    const blockQuoteElement = document.createElement('blockquote');
+    blockQuoteElement.setAttribute("class", "quote");
+    blockQuoteParagraphElement.appendChild(iTagElement);
+    blockQuoteParagraphElement.appendChild(blockQuoteText);
+    blockQuoteElement.appendChild(blockQuoteParagraphElement);
+
+    // create span elements within author attribution
+    const authorSpanElement = document.createElement("span");
+    authorSpanElement.setAttribute("class","name");
+    authorSpanElement.appendChild(document.createTextNode(author));
+    const titleSpanElement = document.createElement("span");
+    titleSpanElement.setAttribute("class","title");
+    titleSpanElement.appendChild(document.createTextNode(title));
+    
+    //add span and other elements to paragraph element in author attribution
+    const paragraphElement = document.createElement('p');
+    paragraphElement.setAttribute("class","source");	
+    paragraphElement.appendChild(authorSpanElement);
+    paragraphElement.appendChild(document.createElement('br'));
+    paragraphElement.appendChild(titleSpanElement);
+
+    // wrap elements into a div and return
+    testimonialElementLi = document.createElement('li');
+    testimonialElementLi.appendChild(blockQuoteElement);
+    testimonialElementLi.appendChild(paragraphElement);
+
+    return testimonialElementLi;
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("filterInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("testimonialUl");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        blockQuote = li[i].getElementsByTagName("blockquote")[0];
+        p = blockQuote.getElementsByTagName("p")[0];
+        txtValue = p.textContent || p.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
