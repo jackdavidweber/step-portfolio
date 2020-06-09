69b511597fbd739ac5e542b7dd83d401dfcf8257
jackdavidweber <jackdavidweber@gmail.com>
2020-06-09 22:52:07 +0000

    add markers for places I've climbed

--- portfolio/src/main/webapp/assets/js/map.js
+++ portfolio/src/main/webapp/assets/js/map.js
@@ -1,4 +1,13 @@
 var map;
+var markers = [
+    {lat: 41.725295, lng: -74.204420 }, // gunks
+    {lat: 22.607288, lng: -83.724946 }, // vinales
+    {lat: 35.668170, lng: -91.724883 }, // jamestown crag
+    {lat: 40.253574, lng: -75.813578 }, // Birdsboro Quarry
+    {lat: 35.221, lng: -114.029 }, // Kingman
+    {lat: 34.565, lng: -117.119 }, // Apple Valley
+    {lat: 40.441, lng: -75.101 }, // High Rocks
+]
 
 var script = document.createElement('script');
 script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA4Cu8t-NfFBtDe7LgTfzqOB2prmSmIQX8&callback=initMap';
@@ -7,9 +16,14 @@ script.async = true;
 
 window.initMap = function(){
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    center: { lat: 39.50, lng: -98.35 },
+    zoom: 3
   });
+
+  for (i=0; i<markers.length; i++){
+      new google.maps.Marker({position: markers[i], map: map});
+  }
+
 }
 
 document.head.appendChild(script);
