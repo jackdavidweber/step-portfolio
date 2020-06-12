99d51013c04e987e595be51436e0a720bf6d2d21
jackdavidweber <jackdavidweber@gmail.com>
2020-06-12 18:33:44 +0000

    first pass getting iterator to work 9 errors still

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,68 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Arrays;
+
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // Create collection of timeranges representing options for meeting
+    Collection<TimeRange> options = Arrays.asList();
+    
+    // check if duration is longer than the day. if so, return empty collection.
+    long duration = request.getDuration();
+    if (duration > TimeRange.WHOLE_DAY.duration()){
+        // options = Arrays.asList();
+        return options;
+    }
+
+    // check if no attendees. if so, return collection of one item being the full day
+    Collection attendees = request.getAttendees();
+    if (attendees.isEmpty()){
+        options = Arrays.asList(TimeRange.WHOLE_DAY);
+        return options;
+    }
+
+    Iterator<Event> eventsIterator =  events.iterator();
+
+
+    if(!eventsIterator.hasNext()){
+        throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    }
+
+    // asuming events contains at least one event
+    Event conflict = eventsIterator.next();
+    System.out.println(conflict);
+    int start = conflict.getWhen().end();
+
+    // check if conflict is NOT at the start of day. if it is not, we know we can add option before it.
+    TimeRange conflictTR = conflict.getWhen();
+    if (!(conflictTR.contains(TimeRange.START_OF_DAY))){
+        options.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, conflict.getWhen().start(), false));
+    } 
+
+    while(eventsIterator.hasNext()){
+        // add the timerange between the end of the last event and the start of the current event
+        conflict = eventsIterator.next();
+        options.add(TimeRange.fromStartEnd(start, conflict.getWhen().start(), false));
+        
+        // set the start for the next option
+        start = conflict.getWhen().end();
+    }
+
+    // check if last conflict is at end of day. If NOT, can add option btwn last conflict and end of day.
+    if(!(conflict.getWhen().contains(TimeRange.END_OF_DAY))){
+        options.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
+    }
+
+    return options;
+
+    // throw new UnsupportedOperationException("TODO: Implement this method.");
+    
+
+    
+
   }
 }
