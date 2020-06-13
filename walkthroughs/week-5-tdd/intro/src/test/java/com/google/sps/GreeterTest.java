9587d16d05174333581c8715f886029db7de6d5e
jackdavidweber <jackdavidweber@gmail.com>
2020-06-13 00:20:18 +0000

    committed the greeter files

--- walkthroughs/week-5-tdd/intro/src/test/java/com/google/sps/GreeterTest.java
+++ walkthroughs/week-5-tdd/intro/src/test/java/com/google/sps/GreeterTest.java
@@ -22,12 +22,23 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public final class GreeterTest {
 
-  @Test
-  public void testGreeting() {
+    @Test
+    public void testGreeting() {
     Greeter greeter = new Greeter();
 
     String greeting = greeter.greet("Ada");
 
     Assert.assertEquals("Hello Ada", greeting);
-  }
+    }
+
+    @Test
+    public void testGreetingTrimsWhitespace() {
+    Greeter greeter = new Greeter();
+
+    String greeting = greeter.greet("   Ada   ");
+
+    // Whitespace should be trimmed
+    Assert.assertEquals("Hello Ada", greeting);
+    }
 }
+
